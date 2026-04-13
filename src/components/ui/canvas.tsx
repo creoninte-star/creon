0164f65ce8c9baf0ca3730cd950984f3b108fb62
Anvin Biju <anvinbiju1@gmail.com>
2026-04-13 12:25:44 +0530

    fix(canvas): resolve TS construct signature error for Node and Line

--- src/components/ui/canvas.tsx
+++ src/components/ui/canvas.tsx
@@ -44,7 +44,7 @@ Line.prototype = {
     // @ts-ignore
     this.nodes = [];
     for (var t, n = 0; n < E.size; n++) {
-      t = new Node();
+      t = new (CanvasNode as any)();
       // @ts-ignore
       t.x = pos.x;
       // @ts-ignore
@@ -121,7 +121,7 @@ function onMousemove(e) {
   function o() {
     lines = [];
     for (let e = 0; e < E.trails; e++)
-      lines.push(new Line({ spring: 0.45 + (e / E.trails) * 0.025 }));
+      lines.push(new (Line as any)({ spring: 0.45 + (e / E.trails) * 0.025 }));
   }
   // @ts-ignore
   function c(e) {
@@ -195,7 +195,7 @@ var ctx,
     dampening: 0.025,
     tension: 0.99,
   };
-function Node(this: any) {
+function CanvasNode(this: any) {
   this.x = 0;
   this.y = 0;
   this.vy = 0;
